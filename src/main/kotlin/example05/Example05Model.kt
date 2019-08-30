e49d392291dc2d804a3ec253744c4c3f6fbfb106
Jeff Butler <jeffgbutler@gmail.com>
2019-08-29 20:56:57 -0400

    Update to new Kotlin pattern

--- src/main/kotlin/example05/Example05Model.kt
+++ src/main/kotlin/example05/Example05Model.kt
@@ -2,6 +2,12 @@ package example05
 
 import java.util.*
 
-data class PersonRecord(val id: Int? = null, val firstName: String? = null, val lastName: String? = null,
-                        val birthDate: Date? = null, val employed: Boolean? = null,
-                        val occupation: String? = null, val addressId: Int? = null)
+data class PersonRecord(
+    val id: Int? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val birthDate: Date? = null,
+    val employed: Boolean? = null,
+    val occupation: String? = null,
+    val addressId: Int? = null
+)
