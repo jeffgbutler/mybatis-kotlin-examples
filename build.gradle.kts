262df43e313e90ef9ad87b07724958305416496f
Jeff Butler <jeffgbutler@gmail.com>
2021-05-07 09:23:10 -0400

    Update Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.0"
 }
 
 group = "com.github.jeffgbutler"
@@ -17,12 +17,12 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation ("org.mybatis:mybatis:3.5.5")
-    implementation ("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
-    testImplementation ("org.assertj:assertj-core:3.17.2")
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testImplementation ("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testImplementation ("org.hsqldb:hsqldb:2.5.1")
+    implementation ("org.mybatis:mybatis:3.5.7")
+    implementation ("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.3.0")
+    testImplementation ("org.assertj:assertj-core:3.19.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation ("org.hsqldb:hsqldb:2.5.2")
 }
 
 tasks.withType<Test> {
