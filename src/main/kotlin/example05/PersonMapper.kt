adcf7564b26a1e3231fb5d94d3ae64d3754c07e0
Jeff Butler <jeffgbutler@gmail.com>
2021-06-08 09:55:50 -0400

    No need for a separate extensions file

--- src/main/kotlin/example05/PersonMapper.kt
+++ src/main/kotlin/example05/PersonMapper.kt
@@ -1,11 +1,35 @@
 package example05
 
+import example05.PersonDynamicSqlSupport.addressId
+import example05.PersonDynamicSqlSupport.birthDate
+import example05.PersonDynamicSqlSupport.employed
+import example05.PersonDynamicSqlSupport.firstName
+import example05.PersonDynamicSqlSupport.id
+import example05.PersonDynamicSqlSupport.lastName
+import example05.PersonDynamicSqlSupport.occupation
+import example05.PersonDynamicSqlSupport.parentId
+import example05.PersonDynamicSqlSupport.person
 import org.apache.ibatis.annotations.Result
 import org.apache.ibatis.annotations.ResultMap
 import org.apache.ibatis.annotations.Results
 import org.apache.ibatis.annotations.SelectProvider
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter
+import org.mybatis.dynamic.sql.util.kotlin.CountCompleter
+import org.mybatis.dynamic.sql.util.kotlin.DeleteCompleter
+import org.mybatis.dynamic.sql.util.kotlin.KotlinUpdateBuilder
+import org.mybatis.dynamic.sql.util.kotlin.SelectCompleter
+import org.mybatis.dynamic.sql.util.kotlin.UpdateCompleter
+import org.mybatis.dynamic.sql.util.kotlin.elements.isEqualTo
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.countFrom
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.deleteFrom
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.insert
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.insertBatch
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.insertMultiple
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.selectDistinct
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.selectList
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.selectOne
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.update
 import org.mybatis.dynamic.sql.util.mybatis3.CommonCountMapper
 import org.mybatis.dynamic.sql.util.mybatis3.CommonDeleteMapper
 import org.mybatis.dynamic.sql.util.mybatis3.CommonInsertMapper
@@ -37,3 +61,145 @@ interface PersonMapper : CommonCountMapper, CommonDeleteMapper, CommonInsertMapp
     @ResultMap("PersonRecordResult")
     fun selectOne(selectStatement: SelectStatementProvider): PersonRecord?
 }
+
+fun PersonMapper.count(completer: CountCompleter) =
+    countFrom(this::count, person, completer)
+
+fun PersonMapper.delete(completer: DeleteCompleter) =
+    deleteFrom(this::delete, person, completer)
+
+fun PersonMapper.deleteByPrimaryKey(id_: Int) =
+    delete {
+        where(id, isEqualTo(id_))
+    }
+
+fun PersonMapper.insert(row: PersonRecord) =
+    insert(this::insert, row, person) {
+        map(id).toProperty("id")
+        map(firstName).toProperty("firstName")
+        map(lastName).toProperty("lastName")
+        map(birthDate).toProperty("birthDate")
+        map(employed).toProperty("employed")
+        map(occupation).toProperty("occupation")
+        map(addressId).toProperty("addressId")
+        map(parentId).toProperty("parentId")
+    }
+
+fun PersonMapper.insertBatch(vararg records: PersonRecord) =
+    insertBatch(records.toList())
+
+fun PersonMapper.insertBatch(records: List<PersonRecord>) =
+    insertBatch(this::insert, records, person) {
+        map(id).toProperty("id")
+        map(firstName).toProperty("firstName")
+        map(lastName).toProperty("lastName")
+        map(birthDate).toProperty("birthDate")
+        map(employed).toProperty("employed")
+        map(occupation).toProperty("occupation")
+        map(addressId).toProperty("addressId")
+        map(parentId).toProperty("parentId")
+    }
+
+fun PersonMapper.insertMultiple(vararg records: PersonRecord) =
+    insertMultiple(records.toList())
+
+fun PersonMapper.insertMultiple(records: List<PersonRecord>) =
+    insertMultiple(
+        this::insertMultiple,
+        records,
+        person
+    ) {
+        map(id).toProperty("id")
+        map(firstName).toProperty("firstName")
+        map(lastName).toProperty("lastName")
+        map(birthDate).toProperty("birthDate")
+        map(employed).toProperty("employed")
+        map(occupation).toProperty("occupation")
+        map(addressId).toProperty("addressId")
+        map(parentId).toProperty("parentId")
+    }
+
+private val columnList = listOf(
+    id.`as`("A_ID"),
+    firstName,
+    lastName,
+    birthDate,
+    employed,
+    occupation,
+    addressId
+)
+
+fun PersonMapper.selectOne(completer: SelectCompleter) =
+    selectOne(
+        this::selectOne,
+        columnList,
+        person,
+        completer
+    )
+
+fun PersonMapper.select(completer: SelectCompleter) =
+    selectList(this::selectMany, columnList, person, completer)
+
+fun PersonMapper.selectDistinct(completer: SelectCompleter) =
+    selectDistinct(
+        this::selectMany,
+        columnList,
+        person,
+        completer
+    )
+
+fun PersonMapper.selectByPrimaryKey(id_: Int) =
+    selectOne {
+        where(id, isEqualTo(id_))
+    }
+
+fun PersonMapper.update(completer: UpdateCompleter) =
+    update(this::update, person, completer)
+
+fun PersonMapper.updateByPrimaryKey(row: PersonRecord) =
+    update {
+        set(firstName).equalToOrNull(row::firstName)
+        set(lastName).equalToOrNull(row::lastName)
+        set(birthDate).equalToOrNull(row::birthDate)
+        set(employed).equalToOrNull(row::employed)
+        set(occupation).equalToOrNull(row::occupation)
+        set(addressId).equalToOrNull(row::addressId)
+        set(parentId).equalToOrNull(row::parentId)
+        where(id, isEqualTo(row.id!!))
+    }
+
+fun PersonMapper.updateByPrimaryKeySelective(row: PersonRecord) =
+    update {
+        set(firstName).equalToWhenPresent(row::firstName)
+        set(lastName).equalToWhenPresent(row::lastName)
+        set(birthDate).equalToWhenPresent(row::birthDate)
+        set(employed).equalToWhenPresent(row::employed)
+        set(occupation).equalToWhenPresent(row::occupation)
+        set(addressId).equalToWhenPresent(row::addressId)
+        set(parentId).equalToWhenPresent(row::parentId)
+        where(id, isEqualTo(row.id!!))
+    }
+
+fun KotlinUpdateBuilder.updateAllColumns(row: PersonRecord) =
+    apply {
+        set(id).equalToOrNull(row::id)
+        set(firstName).equalToOrNull(row::firstName)
+        set(lastName).equalToOrNull(row::lastName)
+        set(birthDate).equalToOrNull(row::birthDate)
+        set(employed).equalToOrNull(row::employed)
+        set(occupation).equalToOrNull(row::occupation)
+        set(addressId).equalToOrNull(row::addressId)
+        set(parentId).equalToOrNull(row::parentId)
+    }
+
+fun KotlinUpdateBuilder.updateSelectiveColumns(row: PersonRecord) =
+    apply {
+        set(id).equalToWhenPresent(row::id)
+        set(firstName).equalToWhenPresent(row::firstName)
+        set(lastName).equalToWhenPresent(row::lastName)
+        set(birthDate).equalToWhenPresent(row::birthDate)
+        set(employed).equalToWhenPresent(row::employed)
+        set(occupation).equalToWhenPresent(row::occupation)
+        set(addressId).equalToWhenPresent(row::addressId)
+        set(parentId).equalToWhenPresent(row::parentId)
+    }
--- src/main/kotlin/example06/GeneratedAlwaysMapper.kt
+++ src/main/kotlin/example06/GeneratedAlwaysMapper.kt
@@ -1,5 +1,10 @@
 package example06
 
+import example06.GeneratedAlwaysDynamicSqlSupport.firstName
+import example06.GeneratedAlwaysDynamicSqlSupport.fullName
+import example06.GeneratedAlwaysDynamicSqlSupport.generatedAlways
+import example06.GeneratedAlwaysDynamicSqlSupport.id
+import example06.GeneratedAlwaysDynamicSqlSupport.lastName
 import org.apache.ibatis.annotations.Flush
 import org.apache.ibatis.annotations.InsertProvider
 import org.apache.ibatis.annotations.Options
@@ -13,6 +18,21 @@ import org.mybatis.dynamic.sql.insert.render.GeneralInsertStatementProvider
 import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter
+import org.mybatis.dynamic.sql.util.kotlin.CountCompleter
+import org.mybatis.dynamic.sql.util.kotlin.DeleteCompleter
+import org.mybatis.dynamic.sql.util.kotlin.KotlinUpdateBuilder
+import org.mybatis.dynamic.sql.util.kotlin.SelectCompleter
+import org.mybatis.dynamic.sql.util.kotlin.UpdateCompleter
+import org.mybatis.dynamic.sql.util.kotlin.elements.isEqualTo
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.countFrom
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.deleteFrom
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.insert
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.insertBatch
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.insertMultipleWithGeneratedKeys
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.selectDistinct
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.selectList
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.selectOne
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.update
 import org.mybatis.dynamic.sql.util.mybatis3.CommonCountMapper
 import org.mybatis.dynamic.sql.util.mybatis3.CommonDeleteMapper
 import org.mybatis.dynamic.sql.util.mybatis3.CommonUpdateMapper
@@ -56,3 +76,114 @@ interface GeneratedAlwaysMapper: CommonCountMapper, CommonDeleteMapper, CommonUp
     @ResultMap("GeneratedAlwaysResult")
     fun selectOne(selectStatement: SelectStatementProvider): GeneratedAlwaysRow?
 }
+
+fun GeneratedAlwaysMapper.count(completer: CountCompleter) =
+    countFrom(this::count, generatedAlways, completer)
+
+fun GeneratedAlwaysMapper.delete(completer: DeleteCompleter) =
+    deleteFrom(this::delete, generatedAlways, completer)
+
+fun GeneratedAlwaysMapper.deleteByPrimaryKey(id_: Int) =
+    delete {
+        where(id, isEqualTo(id_))
+    }
+
+fun GeneratedAlwaysMapper.insert(row: GeneratedAlwaysRow) =
+    insert(
+        this::insert,
+        row,
+        generatedAlways
+    ) {
+        map(firstName).toProperty("firstName")
+        map(lastName).toProperty("lastName")
+    }
+
+fun GeneratedAlwaysMapper.insertBatch(vararg records: GeneratedAlwaysRow) =
+    insertBatch(records.toList())
+
+fun GeneratedAlwaysMapper.insertBatch(records: List<GeneratedAlwaysRow>) =
+    insertBatch(
+        this::insert,
+        records,
+        generatedAlways
+    ) {
+        map(firstName).toProperty("firstName")
+        map(lastName).toProperty("lastName")
+    }
+
+fun GeneratedAlwaysMapper.insertMultiple(vararg records: GeneratedAlwaysRow) =
+    insertMultiple(records.toList())
+
+fun GeneratedAlwaysMapper.insertMultiple(records: List<GeneratedAlwaysRow>) =
+    insertMultipleWithGeneratedKeys(
+        this::insertMultiple,
+        records,
+        generatedAlways
+    ) {
+        map(firstName).toProperty("firstName")
+        map(lastName).toProperty("lastName")
+    }
+
+private val columnList = listOf(
+    id,
+    firstName,
+    lastName,
+    fullName
+)
+
+fun GeneratedAlwaysMapper.selectOne(completer: SelectCompleter) =
+    selectOne(
+        this::selectOne,
+        columnList,
+        generatedAlways,
+        completer
+    )
+
+fun GeneratedAlwaysMapper.select(completer: SelectCompleter) =
+    selectList(this::selectMany, columnList, generatedAlways, completer)
+
+fun GeneratedAlwaysMapper.selectDistinct(completer: SelectCompleter) =
+    selectDistinct(
+        this::selectMany,
+        columnList,
+        generatedAlways,
+        completer
+    )
+
+fun GeneratedAlwaysMapper.selectByPrimaryKey(id_: Int) =
+    selectOne {
+        where(id, isEqualTo(id_))
+    }
+
+fun GeneratedAlwaysMapper.update(completer: UpdateCompleter) =
+    update(
+        this::update,
+        generatedAlways,
+        completer
+    )
+
+fun GeneratedAlwaysMapper.updateByPrimaryKey(row: GeneratedAlwaysRow) =
+    update {
+        set(firstName).equalToOrNull(row::firstName)
+        set(lastName).equalToOrNull(row::lastName)
+        where(id, isEqualTo(row.id!!))
+    }
+
+fun GeneratedAlwaysMapper.updateByPrimaryKeySelective(row: GeneratedAlwaysRow) =
+    update {
+        set(firstName).equalToWhenPresent(row::firstName)
+        set(lastName).equalToWhenPresent(row::lastName)
+        where(id, isEqualTo(row.id!!))
+    }
+
+fun KotlinUpdateBuilder.updateAllColumns(row: GeneratedAlwaysRow) =
+    apply {
+        set(firstName).equalToOrNull(row::firstName)
+        set(lastName).equalToOrNull(row::lastName)
+    }
+
+fun KotlinUpdateBuilder.updateSelectiveColumns(row: GeneratedAlwaysRow) =
+    apply {
+        set(firstName).equalToWhenPresent(row::firstName)
+        set(lastName).equalToWhenPresent(row::lastName)
+    }
